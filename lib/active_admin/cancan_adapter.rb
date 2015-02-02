f6ddd03d348d18394fa0be2fe46dee7a283b7a3e
Nathan Montgomery <nathan.montgomery@spendwellhealth.com>
2015-02-02 13:27:07 -0800

    bump version, correct commit mistake.

--- lib/active_admin/cancan_adapter.rb
+++ lib/active_admin/cancan_adapter.rb
@@ -28,7 +28,7 @@ module ActiveAdmin
     def initialize_cancan_ability
       klass = resource.namespace.cancan_ability_class
       klass = klass.constantize if klass.is_a? String
-      klass.register_ability(AbilityDecorator) if klass.respond_to(:register_ability)
+      klass.register_ability(AbilityDecorator) if klass.respond_to?(:register_ability)
       klass.new user
     end
 
